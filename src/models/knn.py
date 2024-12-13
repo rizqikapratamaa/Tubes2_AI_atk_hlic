85433eb88436bd736f6332489d326e116a5d222e
Aland Mulia Pratama <13522124@std.stei.itb.ac.id>
2024-12-13 19:50:44 +0700

    feat: add KNN Implementations

--- src/models/knn.py
+++ src/models/knn.py
@@ -1,12 +1,14 @@
 import numpy as np
 from collections import Counter
+from concurrent.futures import ThreadPoolExecutor
 
 class KNN:
-    def __init__(self, n_neighbor=3, metric='euclidean', p=3, weighed=False):
+    def __init__(self, n_neighbor=3, metric='euclidean', p=3, weighed=False, n_threads=4):
         self.n_neighbor = n_neighbor
         self.metric = metric
         self.p = p
         self.weighed = weighed
+        self.n_threads = n_threads  # Number of threads to use
 
     def fit(self, X_train, Y_train):
         self.X_train = X_train
@@ -34,7 +36,8 @@ class KNN:
         else:
             return Counter(labels).most_common(1)[0][0]
 
-    def _predict(self, x):
+    def _predict_single(self, x):
+        """Helper function to predict a single sample."""
         distances = []
         for x_train in self.X_train:
             if self.metric == 'euclidean':
@@ -45,27 +48,32 @@ class KNN:
                 distance = self._minkowski_distance(x, x_train, self.p)
             distances.append(distance)
         
-        # Ambil indeks dari k tetangga terdekat
+        # Get indices of the k nearest neighbors
         k_indices = sorted(range(len(distances)), key=lambda i: distances[i])[:self.n_neighbor]
 
-        # Ambil label dari k neighbor terdekat
+        # Get labels of k nearest neighbors
         k_nearest_label = [self.Y_train[i] for i in k_indices]
         k_nearest_distance = [distances[i] for i in k_indices]
 
         return self._most_common_label(k_nearest_label, k_nearest_distance)
 
     def predict(self, X_test):
+        """Predict multiple instances in parallel."""
         predictions = []
-        for x in X_test:
-            predictions.append(self._predict(x))
+        
+        with ThreadPoolExecutor(max_workers=self.n_threads) as executor:
+            # Use threads to predict each test sample in parallel
+            predictions = list(executor.map(self._predict_single, X_test))
+        
         return predictions
-    
+
     def get_params(self, deep=False):
         return {
             "n_neighbor": self.n_neighbor,
             "metric": self.metric,
             "p": self.p,
-            "weighed": self.weighed
+            "weighed": self.weighed,
+            "n_threads": self.n_threads
         }
 
     def set_params(self, **params):
